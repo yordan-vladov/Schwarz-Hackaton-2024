88fb963ea2d5da4fb51bd09df70ef25ce01727a6
Emil Korudzhiev <e.korudzhiev@abv.bg>
2024-06-28 01:01:27 +0300

    ТУЕС👊🏿💥

--- server/src/main/java/dev/uktcteam/hackathon/pathfinding/PathfindingController.java
+++ server/src/main/java/dev/uktcteam/hackathon/pathfinding/PathfindingController.java
@@ -1,10 +1,7 @@
 package dev.uktcteam.hackathon.pathfinding;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/pathfind")
@@ -15,7 +12,7 @@ public class PathfindingController {
 
     @GetMapping("{storeId}")
     public PathfindDto findPath(
-            @RequestBody String[] products
+            @RequestParam("products") String[] products
     ) {
         return pathfindingService.findPath(products);
     }
